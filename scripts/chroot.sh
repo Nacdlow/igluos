e3ad079377d051b2f622fca20f8685a76749311b
Humaid AlQassimi <git@humaidq.ae>
2020-02-28 17:29:36 +0000

    Generate eduroam wpa_supplicant network.

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -1,4 +1,42 @@
 #!/bin/bash
+echo -n "Enter your eduroam username (with domain): "
+read WIFI_USERNAME
+echo -n "Enter your eduroam password (will not echo): "
+read -s WIFI_PASSWORD
+
+
+cat > wpa_conf.txt <<EOF
+network={
+	ssid="eduroam"
+	proto=RSN
+	key_mgmt=WPA-EAP
+	pairwise=CCMP
+	auth_alg=OPEN
+	eap=PEAP
+	identity="${WIFI_USERNAME}"
+	password="${WIFI_PASSWORD}"
+	phase2="auth=MSCHAPV2"
+	disabled=0
+}
+EOF
+
+cat wpa_conf.txt | sudo tee -a $CHROOT/etc/wpa_supplicant/wpa_supplicant.conf
+rm wpa_conf.txt
+
+sudo proot -q qemu-arm-static \
+	-r ${PWD}/${CHROOT}/ \
+	-b /proc/ \
+	-b /dev/ \
+	-b /sys/ \
+	-b /etc/resolv.conf \
+	-b /usr/bin/qemu-arm-static \
+	-0 \
+	qemu-arm-static /bin/bash <<EOF
+export LC_ALL=C
+#apt update && apt upgrade-y
+uname -a
+EOF
+
 
 sudo proot -q qemu-arm-static \
 	-r ${PWD}/${CHROOT}/ \
